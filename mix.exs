a2dfe2c1c691c07d82580d762fbbe70cc7a505d4
meddle <meddlesmerch@gmail.com>
2017-01-29 17:28:40 +0200

    Fix docs to publish.

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Blogit.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      preferred_cli_env: [espec: :test],
+     docs: [readme: true, main: "README.md"],
      description: """
        Blogit is an OTP application for generating blog posts from a git
        repository containing markdown files.
@@ -26,6 +27,7 @@ defmodule Blogit.Mixfile do
       {:git_cli, "~> 0.2"},
       {:earmark, "~> 1.0.3"},
       {:yaml_elixir, "~> 1.3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:espec, "~> 1.2.1", only: :test}
     ]
   end
@@ -34,7 +36,7 @@ defmodule Blogit.Mixfile do
     %{
       licenses: ["MIT"],
       maintainers: ["Nikolay Tsvetinov (Meddle)"],
-      links: [%{"GitHub" => "https://github.com/meddle0x53/blogit"}]
+      links: %{"GitHub" => "https://github.com/meddle0x53/blogit"}
     }
   end
 end

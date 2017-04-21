7b466ec422ca4c6a57228ab3a7a0212df78cd022
meddle <meddlesmerch@gmail.com>
2017-04-22 02:08:17 +0300

    Remove espec.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule Blogit.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     preferred_cli_env: [espec: :test],
      docs: [readme: true, main: "README.md"],
      description: """
        Blogit is an OTP application for generating blog posts from a git
@@ -28,8 +27,7 @@ defmodule Blogit.Mixfile do
       {:earmark, "~> 1.1"},
       {:yaml_elixir, "~> 1.3.0"},
       {:calendar, "~> 0.16.1"},
-      {:ex_doc, ">= 0.15.0", only: :dev},
-      {:espec, "~> 1.2.1", only: :test}
+      {:ex_doc, ">= 0.15.0", only: :dev}
     ]
   end
 

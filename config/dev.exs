101c04e328e59514904c29fbb1d85de343c257a8
9to1url <60271575+9to1url@users.noreply.github.com>
2020-03-07 10:19:13 -0500

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :logger, level: :error
 
 config :blogit,
-  repository_url: "/Users/Shared/adata/erlang/elixir/blogit_sample",
+  repository_url: "git@github.com:meddle0x53/elixir-blog.git",
   polling: true,
   poll_interval: 5,
   languages: ~w(bg en),
